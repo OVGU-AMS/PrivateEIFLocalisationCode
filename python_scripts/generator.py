Fixed approximated variance trying to correct for non zero mean estimate (it risked negative values and cause the filter to break) Made python script that generates, runs and interprets simulation - handles the fact that python files are in a sub-directory Regenerated input but in smaller quantity for testing
Typo in sim

python_scripts/generator.py
@@ -2,8 +2,8 @@
 
 """
 
+import os
 import numpy as np
-
 
 
 # These may remain constant, use num_sensors and first_sensor_index to select the sensor subset
@@ -26,18 +26,24 @@ SENSOR_LOCATIONS = [np.array([5.0, 5.0]), # Normal
                     np.array([23.0, 23.0])]
 
 # Defaults when this file run
-TRACK_FILEPATH_DEFAULT = "../input/default_track1.txt"
-SENSOR_FILEPATH_BASE_DEFAULT = "../input/debug_sensor%d.txt"
-NUM_SIMS_DEFAULT = 100
-NUM_SENSORS_DEFAULT = 4
+TRACK_FILEPATH_DEFAULT = "input/debug_track1.txt"
+SENSOR_FILEPATH_BASE_DEFAULT = "input/debug_sim_%03d_sensor%d.txt"
+NUM_SIMS_DEFAULT = 1
 FIRST_SENSOR_INDEX_DEFAULT = 0
+NUM_SENSORS_DEFAULT = 4
 
 
 # generate inputs for a particular simulation setup
 def generate_sim_inputs(track_filepath, sensor_filepath_base, number_of_sims, first_sensor_index, number_of_sensors):
+    # Always run from top project folder, if currently in python folder, move up
+    dir_moved = False
+    if os.getcwd().endswith('python_scripts'):
+        os.chdir('../')
+        dir_moved = True
+
     for s in range(1, number_of_sims+1):
         track_f = open(track_filepath, 'r')
-        sensor_fs = [open(sensor_filepath_base % (s, i), 'w') for i in range(1, number_of_sensors+1)]
+        sensor_fs = [open(sensor_filepath_base % (s, str(i)), 'w') for i in range(1, number_of_sensors+1)]
         all_sensor_measurements = [[] for i in range(number_of_sensors)]
 
         timesteps = int(track_f.readline())
@@ -61,6 +67,11 @@ def generate_sim_inputs(track_filepath, sensor_filepath_base, number_of_sims, fi
         for i in range(number_of_sensors):
             sensor_fs[i].write('\n'.join(all_sensor_measurements[i]))
             sensor_fs[i].close()
+
+    # If directory was moved up, move it back down before ending
+    if dir_moved:
+        os.chdir('./python_scripts')
+
     return
 
 

python_scripts/plotter.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # Defaults when this file is run
 TRACK_FILEPATH_DEFAULT = "../input/track1.txt"
-OUTPUT_FILEPATH_DEFAULT = "../output/encoding_nav001.txt"
+OUTPUT_FILEPATH_DEFAULT = "../output/encoding_nav006.txt"
 SENSOR_LOCATIONS_DEFAULT = [np.array([5.0, 5.0]), # Normal
                             np.array([40.0, 5.0]), 
                             np.array([5.0, 40.0]), 
@@ -34,8 +34,8 @@ def plot_sim(track_filepath, output_filepath, sensor_locations=None, num_sensors
     states = []
     covariances = []
 
-    with open("../input/track1.txt") as in_f:
-        with open("../output/encoding_nav001.txt") as out_f:
+    with open(track_filepath) as in_f:
+        with open(output_filepath) as out_f:
             t = int(in_f.readline())
             dim = int(in_f.readline())
             init_state = np.array([float(x) for x in in_f.readline().split()])